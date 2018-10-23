1b50dc6d25b0b6457ae5a69b1a1c0cae28c7287d
nielsdelestinne <nielsdelestinne@gmail.com>
2018-10-23 15:03:17 +0200

    Movie rental added
    Former-commit-id: f621d3447a807e3b2068a2419d781dc09873ffa5 [formerly 54a12c581f785d23e48e09a88ebcc861d0ae19ba]
    Former-commit-id: 7340956a58643e5fa5adb2fa9a6f12da63c8b8bd

---
+++ 20-programming-advanced-java/05-agile-software-development/20-refactoring-clean-code/src/main/java/codelab03/Rental.java
@@ -0,0 +1,20 @@
+package codelab03;
+
+public class Rental {
+
+    private final Movie movie;
+    private final int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+}
