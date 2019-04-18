f3751ef4221edabb6612fafbbda2004c1c834353
nielsdelestinne <nielsdelestinne@gmail.com>
2019-04-18 09:55:05 +0200

    object and classes presentation code snippets (some more)
    Former-commit-id: e2bbbb249c2b56949153e58c5fced79e7608deff [formerly 74009f05058a0a842e661fc4930c80005c8427f6]
    Former-commit-id: 99560a9b34748aee44dd7a7fd85826097ec60339

--- 10-programming-fundamentals-java/10-basics/00-presentation-code-examples/src/main/java/presentationslides/classeschapter/cat01/CatApplication.java
+++ 10-programming-fundamentals-java/10-basics/00-presentation-code-examples/src/main/java/presentationslides/classeschapter/cat01/CatApplication.java
@@ -3,7 +3,7 @@ package presentationslides.classeschapter.cat01;
 public class CatApplication {
 
     public static void main(String[] args) {
-        Cat garfield = new Cat("Pissed", true, 9, 30, "Orange");
+        Cat garfield = new Cat("Garfield","Pissed");
         String producedSound = garfield.meow();
         System.out.println("This is what garfield sounds like: " + producedSound);
 
