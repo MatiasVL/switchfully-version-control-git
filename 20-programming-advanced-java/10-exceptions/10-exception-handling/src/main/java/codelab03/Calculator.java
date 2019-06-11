45760b6d3f4fd81a699d9eaf22573a0573f0bf4b
nielsdelestinne <nielsdelestinne@gmail.com>
2019-06-11 15:56:47 +0200

    package number from 02 to 03
    Former-commit-id: 3b4b8dd272c5c331225257e66725dede1edee00a [formerly 3b46431052297d656c7441e57385155f57630d3e]
    Former-commit-id: 487891506c792e621df3760e6c30a9e3d494ac0a

--- 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/codelab03/Calculator.java
+++ 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/codelab03/Calculator.java
@@ -1,4 +1,4 @@
-package codelab02;
+package codelab03;
 
 import java.util.Scanner;
 
--- 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/codelab03/CalculatorService.java
+++ 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/codelab03/CalculatorService.java
@@ -1,4 +1,4 @@
-package codelab02;
+package codelab03;
 
 /**
  * You don't have to change anything in this class.
