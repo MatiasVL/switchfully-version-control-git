07de36fc6906d2671903fc390b2e6f5214accd4f
nielsdelestinne <nielsdelestinne@gmail.com>
2019-06-13 11:11:44 +0200

    20-exceptions-combined: Codelab06 (files) added
    Former-commit-id: 65db0ce9a67ad1158b215fbffce856440f744adc [formerly c66cfc7687d6018738b3c07d8c7a994098d81a99]
    Former-commit-id: 01eedaad2574321fa8952d882169467487cea208

---
+++ 20-programming-advanced-java/10-exceptions/20-exceptions-combined/src/main/java/codelab06/MyFileReader.java
@@ -0,0 +1,9 @@
+package codelab06;
+
+class MyFileReader {
+
+    public void read(String fileName) {
+        // implement me...
+    }
+
+}
---
+++ 20-programming-advanced-java/10-exceptions/20-exceptions-combined/src/main/java/codelab06/ReaderApplication.java
@@ -0,0 +1,14 @@
+package codelab06;
+
+public class ReaderApplication {
+
+    public static void main(String[] args) {
+
+        MyFileReader myFileReader = new MyFileReader();
+        myFileReader.read("example.txt");
+            System.out.println("========================");
+        myFileReader.read("scores.csv");
+
+    }
+
+}
