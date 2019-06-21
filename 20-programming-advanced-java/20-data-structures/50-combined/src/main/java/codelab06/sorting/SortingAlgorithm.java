6a2b0ffe37eff3d864d1c6469f5bd16a6ef37482
nielsdelestinne <nielsdelestinne@gmail.com>
2019-06-21 15:19:18 +0200

    20-Data-structures: added all remaining codelabs
    Former-commit-id: b94f3ba2a8617cff7e5b10b2c944d611a607e38e [formerly 1d45970c5b112fa10d409c86f95159f084f46044]
    Former-commit-id: 8839e630b33e29a0155d64ed340a5ffd08e8ef11

---
+++ 20-programming-advanced-java/20-data-structures/50-combined/src/main/java/codelab06/sorting/SortingAlgorithm.java
@@ -0,0 +1,9 @@
+package codelab06.sorting;
+
+import java.util.List;
+
+public interface SortingAlgorithm {
+
+    List<Integer> sort(List<Integer> listToSort);
+
+}
---
+++ 20-programming-advanced-java/20-data-structures/50-combined/src/main/java/codelab07/BinarySearch.java
@@ -0,0 +1,12 @@
+package codelab07;
+
+import java.util.List;
+
+public class BinarySearch {
+
+    // Make sure to sort before searching!
+    boolean isElementInList(int elementToSearch, List<Integer> listToSearchFrom) {
+        throw new UnsupportedOperationException("Implement me");
+    }
+
+}
