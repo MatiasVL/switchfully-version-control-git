3feda98ac6b576bbe8c339f150fdb331682093fc
nielsdelestinne <nielsdelestinne@gmail.com>
2019-06-25 11:09:16 +0200

    30-Generics: 10-generic-classes: 2 codelabs provided
    Former-commit-id: 0907c6fcfcb4cd74daa02a462e45ae5124d23aa0 [formerly 566e50dd1acf6bc8685aa36fca1d9c94cd10b67c]
    Former-commit-id: c0c982f99c5eb893284bfafacbfcded4f70a5d93

---
+++ 20-programming-advanced-java/30-generics/10-generic-classes/src/main/java/codelab01/Box.java
@@ -0,0 +1,14 @@
+package codelab01;
+
+public class Box {
+
+    private Object contentOfBox;
+
+    void store(Object contentOfBox) {
+        this.contentOfBox = contentOfBox;
+    }
+
+    Object getContentOfBox() {
+        return contentOfBox;
+    }
+}
