341b769c395b9c42fd7385fa94495e560bf5e673
vercruyssetim <vercruysse.tim@gmail.com>
2019-07-29 12:59:08 +0200

    timve:JPA - some config corrections
    Former-commit-id: 05d6ad95608aaeddd9f308a261bc951aff28083a [formerly 1ce9c64944a13e4bd17178a2f8fb3668208f6e20]
    Former-commit-id: e89f50500e0562eb3e2e7d3b8fbb94cff4b80ee1

--- 40-databases-orm-java/10-jpa/src/main/java/config/DatasourceConfig.java
+++ 40-databases-orm-java/10-jpa/src/main/java/config/DatasourceConfig.java
@@ -9,7 +9,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("database.properties")
+@PropertySource("classpath:database.properties")
 public class DatasourceConfig {
 
     private final Environment environment;
