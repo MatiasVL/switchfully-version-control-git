19c9e4a6b77a478cc43a0ec56601613501bdcf7a
nielsjani <niels.jani@cegeka.com>
2019-08-30 16:54:36 +0200

    NielsJ: oauth exercise solution ui

---
+++ 30-enterprise-software-development-java/40-security/10-spring-security/switchfully-oauth-solution/author-ui/src/app/books/books.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {BookService} from '../book.service';
+import {AuthService} from '../auth.service';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+
+  books$: Observable<any[]>;
+  rated = [];
+
+  constructor(private bookService: BookService, private authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.books$ = this.bookService.getAllBooks();
+  }
+
+  rate(bookId, amount) {
+    this.rated.push(bookId);
+    this.bookService.rate(bookId, {scoreOutOfFive: amount, raterName: 'Ratey McRateFace'})
+      .subscribe(() => console.log('Rating successful'));
+  }
+
+  canAddBooks() {
+    return this.authService.hasPermission('create:book');
+  }
+}
