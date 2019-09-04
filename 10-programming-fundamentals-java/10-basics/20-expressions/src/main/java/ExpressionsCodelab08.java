c3dd964c317dfaf7288255dba1ee76be9b744276
vercruyssetim <tim.vercruysse@cegeka.be>
2019-09-04 13:17:37 +0200

    removed exercise 7 with was a double of 8

--- 10-programming-fundamentals-java/10-basics/20-expressions/src/main/java/ExpressionsCodelab08.java
+++ 10-programming-fundamentals-java/10-basics/20-expressions/src/main/java/ExpressionsCodelab08.java
@@ -7,9 +7,9 @@ public class ExpressionsCodelab08 {
         System.out.println("--------------");
 
         // ----------------
-        // Create a double variable that holds the division of 2 other int variables (with values 9 and 2)
+        // Create a double variable that holds the division of an int and a double variable (with values 9 and 2)
         // Print the result using: System.out.println("Printing division: " + <YOUR_DIVISION_VARIABLE_NAME>);
-        // Inspect the result, does it look as desired? Weren't we expecting something else?
+        // Inspect the result, does it look as desired?
 
     }
 
