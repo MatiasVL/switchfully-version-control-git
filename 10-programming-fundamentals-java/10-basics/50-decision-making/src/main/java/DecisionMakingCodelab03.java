369842f0bc78f833d3bb32b71abd9ba36aeacb9b
nielsdelestinne <nielsdelestinne@gmail.com>
2019-09-05 12:02:04 +0200

    Fixed methods name in comments to match actual method name

--- 10-programming-fundamentals-java/10-basics/50-decision-making/src/main/java/DecisionMakingCodelab03.java
+++ 10-programming-fundamentals-java/10-basics/50-decision-making/src/main/java/DecisionMakingCodelab03.java
@@ -8,9 +8,9 @@ public class DecisionMakingCodelab03 {
      * Given 3 int values, a b c, return their sum.
      * However, if one of the values is the same as another of the values,
      * it does not count towards the sum.
-     *      loneSum(1, 2, 3) → 6
-     *      loneSum(3, 2, 3) → 2
-     *      loneSum(3, 3, 3) → 0
+     *      uniqueSum(1, 2, 3) → 6
+     *      uniqueSum(3, 2, 3) → 2
+     *      uniqueSum(3, 3, 3) → 0
     */
     public static int uniqueSum(int operand1, int operand2, int operand3) {
         throw new UnsupportedOperationException("Implement me");
--- 10-programming-fundamentals-java/10-basics/50-decision-making/src/main/java/DecisionMakingCodelab04.java
+++ 10-programming-fundamentals-java/10-basics/50-decision-making/src/main/java/DecisionMakingCodelab04.java
@@ -8,9 +8,9 @@ public class DecisionMakingCodelab04 {
      * Given 2 int values greater than 0,
      * return whichever value is nearest to 21 without going over.
      * Return 0 if they both go over.
-     *      blackjack(19, 21) → 21
-     *      blackjack(21, 19) → 21
-     *      blackjack(19, 22) → 19
+     *      playBlackjack(19, 21) → 21
+     *      playBlackjack(21, 19) → 21
+     *      playBlackjack(19, 22) → 19
      */
     public static int playBlackjack(int cardValue1, int cardValue2) {
         throw new UnsupportedOperationException("Implement me");
