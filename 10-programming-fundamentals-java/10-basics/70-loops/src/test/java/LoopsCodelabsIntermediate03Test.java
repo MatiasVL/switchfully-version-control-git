b8e59c63b2da8a9794ef45e0cc84e1c3a7de30a5
vercruyssetim <tim.vercruysse@cegeka.be>
2019-09-06 14:16:02 +0200

    extra intermediate exercise

---
+++ 10-programming-fundamentals-java/10-basics/70-loops/src/test/java/LoopsCodelabsIntermediate03Test.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LoopsCodelabsIntermediate03Test {
+
+    @Test
+    void differentLength() {
+        assertThat(LoopsCodelabsIntermediate03.merge(new int[]{1}, new int[]{1, 2, 3})).isEmpty();
+    }
+
+    @Test
+    void simpleMerge() {
+        assertThat(LoopsCodelabsIntermediate03.merge(new int[]{1, 2, 3}, new int[]{1, 2, 3})).contains(1, 1, 2, 2, 3, 3);
+    }
+
+    @Test
+    void advancedMerge() {
+        assertThat(LoopsCodelabsIntermediate03.merge(new int[]{3, 5, 15, 236, 78, 45, 95}, new int[]{1, 2, 3, 4, 6, 8, -10})).contains(1, 1, 2, 2, 3, 3);
+    }
+}
