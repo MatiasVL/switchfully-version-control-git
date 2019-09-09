65974d2ff75f2d3d24297938a49b46985726dabc
nielsdelestinne <nielsdelestinne@gmail.com>
2019-09-09 07:23:14 +0200

    Fixed zipper test (advanced)

--- 10-programming-fundamentals-java/10-basics/70-loops/src/test/java/LoopsCodelabsIntermediate03Test.java
+++ 10-programming-fundamentals-java/10-basics/70-loops/src/test/java/LoopsCodelabsIntermediate03Test.java
@@ -16,6 +16,7 @@ class LoopsCodelabsIntermediate03Test {
 
     @Test
     void advancedMerge() {
-        assertThat(LoopsCodelabsIntermediate03.merge(new int[]{3, 5, 15, 236, 78, 45, 95}, new int[]{1, 2, 3, 4, 6, 8, -10})).contains(1, 1, 2, 2, 3, 3);
+        assertThat(LoopsCodelabsIntermediate03.merge(new int[]{3, 5, 15, 236, 78, 45, 95}, new int[]{1, 2, 3, 4, 6, 8, -10}))
+                .contains(3, 1, 5, 2, 15, 3, 236, 4, 78, 6, 45, 8, 95, -10);
     }
 }
