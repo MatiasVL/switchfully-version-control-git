18cf588643d851d4c1e838b17279bc74819835c9
nielsdelestinne <nielsdelestinne@gmail.com>
2019-09-09 10:38:25 +0200

    Corrected wrongly spelled fibonaci to fibonacci

--- 10-programming-fundamentals-java/10-basics/70-loops/src/main/java/LoopsCodelabs09.java
+++ 10-programming-fundamentals-java/10-basics/70-loops/src/main/java/LoopsCodelabs09.java
@@ -1,18 +1,18 @@
 public class LoopsCodelabs09 {
 
     /**
-     * Create a method that will return the n-th fibonaci number.
+     * Create a method that will return the n-th fibonacci number.
      * E.g.
-     * fibonaci(1) = 1
-     * fibonaci(2) = 1
-     * fibonaci(3) = 2
-     * fibonaci(4) = 3
-     * fibonaci(5) = 5
-     * fibonaci(6) = 8
+     * fibonacci(1) = 1
+     * fibonacci(2) = 1
+     * fibonacci(3) = 2
+     * fibonacci(4) = 3
+     * fibonacci(5) = 5
+     * fibonacci(6) = 8
      * ...
-     * fibonaci(n) = fibonaci(n - 1) + fibonaci (n - 2)
+     * fibonacci(n) = fibonacci(n - 1) + fibonacci (n - 2)
      */
-    public static int fibonaci(int number) {
+    public static int fibonacci(int number) {
         return 0;
     }
 }
--- 10-programming-fundamentals-java/10-basics/70-loops/src/test/java/LoopsCodelabs09Test.java
+++ 10-programming-fundamentals-java/10-basics/70-loops/src/test/java/LoopsCodelabs09Test.java
@@ -5,16 +5,16 @@ class LoopsCodelabs09Test {
 
     @Test
     void negativeOrZeroNumber() {
-        Assertions.assertThat(LoopsCodelabs09.fibonaci(-5)).isEqualTo(0);
-        Assertions.assertThat(LoopsCodelabs09.fibonaci(0)).isEqualTo(0);
+        Assertions.assertThat(LoopsCodelabs09.fibonacci(-5)).isEqualTo(0);
+        Assertions.assertThat(LoopsCodelabs09.fibonacci(0)).isEqualTo(0);
     }
 
     @Test
-    void checkFibonaci() {
-        Assertions.assertThat(LoopsCodelabs09.fibonaci(1)).isEqualTo(1);
-        Assertions.assertThat(LoopsCodelabs09.fibonaci(2)).isEqualTo(1);
-        Assertions.assertThat(LoopsCodelabs09.fibonaci(5)).isEqualTo(5);
-        Assertions.assertThat(LoopsCodelabs09.fibonaci(8)).isEqualTo(21);
-        Assertions.assertThat(LoopsCodelabs09.fibonaci(20)).isEqualTo(6765);
+    void checkFibonacci() {
+        Assertions.assertThat(LoopsCodelabs09.fibonacci(1)).isEqualTo(1);
+        Assertions.assertThat(LoopsCodelabs09.fibonacci(2)).isEqualTo(1);
+        Assertions.assertThat(LoopsCodelabs09.fibonacci(5)).isEqualTo(5);
+        Assertions.assertThat(LoopsCodelabs09.fibonacci(8)).isEqualTo(21);
+        Assertions.assertThat(LoopsCodelabs09.fibonacci(20)).isEqualTo(6765);
     }
 }
