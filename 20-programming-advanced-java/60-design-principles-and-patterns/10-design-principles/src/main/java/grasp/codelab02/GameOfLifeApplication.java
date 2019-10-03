9aee3527e8d4a6af9dcba27eba84bcdb9135e689
vercruyssetim <tim.vercruysse@cegeka.be>
2019-10-03 13:39:43 +0200

    put back the game of life exercise

---
+++ 20-programming-advanced-java/60-design-principles-and-patterns/10-design-principles/src/main/java/grasp/codelab02/GameOfLifeApplication.java
@@ -0,0 +1,39 @@
+package grasp.codelab02;
+
+import static grasp.codelab02.GameOfLife.life;
+import static grasp.codelab02.GameOfLife.print;
+
+/**
+ * We have provided this class for when
+ * you want to manually validate and run your life method.
+ * To 'try out' your implementation.
+ */
+public class GameOfLifeApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        String[] dish= {
+                "-X-----------",
+                "--X----------",
+                "XXX----------",
+                "-------------",
+                "-------------",
+                "-------------",
+                "-------------",
+                "-------------",
+                "-------------",
+                "-------------",
+                "-------------",
+                "-------------",
+                "-------------"
+        };
+        int gens= 20;
+        for(int i= 0;i < gens;i++){
+            System.out.println("\r\n\r\n\r\n");
+            System.out.println("Generation " + i + ":");
+            print(dish);
+            dish= life(dish);
+            Thread.sleep(1000);
+        }
+    }
+
+}
