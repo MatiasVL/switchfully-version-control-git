d7aef90a9ed8eb1451ecc0f368f89ef6e8ed6c87
nielsdelestinne <nielsdelestinne@gmail.com>
2019-10-11 11:41:13 +0200

    Fix mistake: new object, not a new mock object

--- 30-enterprise-software-development-java/40-security/10-spring-security/2-operation-baobab/src/test/java/com/cegeka/switchfully/security/ArmyAuthenticationEntryPointTest.java
+++ 30-enterprise-software-development-java/40-security/10-spring-security/2-operation-baobab/src/test/java/com/cegeka/switchfully/security/ArmyAuthenticationEntryPointTest.java
@@ -1,7 +1,6 @@
 package com.cegeka.switchfully.security;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
