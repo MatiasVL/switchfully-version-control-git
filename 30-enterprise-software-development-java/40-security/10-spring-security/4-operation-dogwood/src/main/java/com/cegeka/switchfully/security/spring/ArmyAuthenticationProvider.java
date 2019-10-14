95a6b31dd285d7f576fe58ed47f5d577c6669a45
nielsdelestinne <nielsdelestinne@gmail.com>
2019-10-14 10:06:47 +0200

    Refactoring + cleanup of class ArmyAuthenticationProvider and SecurityConfig

--- 30-enterprise-software-development-java/40-security/10-spring-security/4-operation-dogwood/src/main/java/com/cegeka/switchfully/security/spring/ArmyAuthenticationProvider.java
+++ 30-enterprise-software-development-java/40-security/10-spring-security/4-operation-dogwood/src/main/java/com/cegeka/switchfully/security/spring/ArmyAuthenticationProvider.java
@@ -8,25 +8,42 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ArmyAuthenticationProvider implements AuthenticationProvider {
 
+    private final FakeAuthenticationService fakeAuthenticationService;
+
     @Autowired
-    private FakeAuthenticationService fakeAuthenticationService;
+    public ArmyAuthenticationProvider(FakeAuthenticationService fakeAuthenticationService) {
+        this.fakeAuthenticationService = fakeAuthenticationService;
+    }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         ExternalAuthenticaton user = fakeAuthenticationService.getUser(authentication.getPrincipal().toString(), authentication.getCredentials().toString());
         if(user != null){
-            return new ArmyAuthentication(user.getUsername(), user.getPassword(), user.getRoles());
+            return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), rolesToGrantedAuthorities(user.getRoles()));
         }
-        throw new BadCredentialsException("BAD BAD NOT GOOD");
+        throw new BadCredentialsException("The provided credentials were invalid.");
+
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
         return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+
+    private Collection<? extends GrantedAuthority> rolesToGrantedAuthorities(List<String> roles) {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 }
--- 30-enterprise-software-development-java/40-security/10-spring-security/4-operation-dogwood/src/main/java/com/cegeka/switchfully/security/spring/SecurityConfig.java
+++ 30-enterprise-software-development-java/40-security/10-spring-security/4-operation-dogwood/src/main/java/com/cegeka/switchfully/security/spring/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cegeka.switchfully.security.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,43 +14,30 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @Configuration
 @EnableWebSecurity
-//annotation needed for spring to pick up the @PreAuthorize annotations on the ArmyResource
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final AuthenticationEntryPoint authEntryPoint;
+    private final AuthenticationProvider armyAuthenticationProvider;
+
     @Autowired
-    private AuthenticationEntryPoint authEntryPoint;
-    @Autowired
-    private ArmyAuthenticationProvider armyAuthenticationProvider;
+    public SecurityConfig(AuthenticationEntryPoint authEntryPoint, AuthenticationProvider armyAuthenticationProvider) {
+        this.authEntryPoint = authEntryPoint;
+        this.armyAuthenticationProvider = armyAuthenticationProvider;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.csrf().disable().authorizeRequests()
-                //One way to fix the authorisation problem is using the 'antmatchers' methods to force users to have certain role(s) if they want to access a certain endpoint
-                //advantage: able to secure multiple, similar url's at the same time
-                //disadvantage: this code is completely decoupled from the Rest-controller code. This makes it easy to forget to adjust it when e.g. adding a new rest-call
-//                .antMatchers("/army").hasRole("CIVILIAN")
-//                .antMatchers("/army/promote/**").hasRole("HUMAN_RELATIONSHIPS")
-//                .antMatchers("/army/discharge/**").hasRole("HUMAN_RELATIONSHIPS")
-//                .antMatchers("/army/nuke").hasRole("GENERAL")
-//                .antMatchers("/army/**").hasAnyRole("PRIVATE", "GENERAL")
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .authenticationEntryPoint(authEntryPoint);
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(armyAuthenticationProvider);
-//        auth.inMemoryAuthentication()
-//                .withUser("ZWANETTA").password("WORST").roles("CIVILIAN")
-//                .and()
-//                .withUser("JMILLER").password("THANKS").roles("PRIVATE")
-//                .and()
-//                .withUser("UNCLE").password("SAM").roles("HUMAN_RELATIONSHIPS")
-//                .and()
-//                .withUser("GENNY").password("RALLY").roles("GENERAL");
     }
 
 }
