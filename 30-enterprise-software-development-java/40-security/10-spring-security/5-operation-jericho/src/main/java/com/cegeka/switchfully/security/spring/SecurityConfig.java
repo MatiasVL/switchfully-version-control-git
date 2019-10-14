61cd7ade5653316bd5b2e345254be6be9c882407
nielsdelestinne <nielsdelestinne@gmail.com>
2019-10-14 21:03:27 +0200

    Added unsecure get information api

--- 30-enterprise-software-development-java/40-security/10-spring-security/5-operation-jericho/src/main/java/com/cegeka/switchfully/security/spring/SecurityConfig.java
+++ 30-enterprise-software-development-java/40-security/10-spring-security/5-operation-jericho/src/main/java/com/cegeka/switchfully/security/spring/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cegeka.switchfully.security.spring;
 
+import com.cegeka.switchfully.security.rest.InformationResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -29,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.csrf().disable().authorizeRequests()
+                .antMatchers(InformationResource.INFORMATION_RESOURCE_PATH).permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .authenticationEntryPoint(authEntryPoint);
