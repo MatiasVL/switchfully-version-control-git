265af4eb1c3311c5aad7449dcb04666760cdca5c
vercruyssetim <tim.vercruysse@cegeka.be>
2020-01-16 09:56:15 +0100

    moved code example to its own repository

--- 30-enterprise-software-development-java/20-enterprise-framework/10-spring/code_example01/src/main/java/com/switchfully/spring/tax/system/TaxSystem.java
+++
@@ -1,8 +0,0 @@
-package com.switchfully.spring.tax.system;
-
-import com.switchfully.spring.person.Person;
-import com.switchfully.spring.tax.Taxation;
-
-public interface TaxSystem {
-    Taxation calculateTaxFor(Person person, double income);
-}
--- 30-enterprise-software-development-java/20-enterprise-framework/10-spring/code_example01/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into PERSON (ID, FIRST_NAME, LAST_NAME, JOB) values (1, 'Bob', 'Dylan', 'SINGER');
-insert into PERSON (ID, FIRST_NAME, LAST_NAME, JOB) values (2, 'Bob', 'Ross', 'PAINTER');
-insert into PERSON (ID, FIRST_NAME, LAST_NAME, JOB) values (3, 'Bob', 'Marley', 'SINGER');
-insert into PERSON (ID, FIRST_NAME, LAST_NAME, JOB) values (4, 'Steve', 'Jobs', 'MANAGER');
